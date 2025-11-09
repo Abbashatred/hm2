5eb5e16d6a2795f0dab32facb0ff2c30ea1b4fdb
Abbashatred <gitabbas26@gmail.com>
2025-11-09 19:45:11 +0400

    upd hm2

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import json
+import utils
+
+app = Flask(__name__)
+
+with open("candidates.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    
+    
---
+++ utils.py
@@ -0,0 +1,16 @@
+# load_candidates_from_j son(path) - возвращает список всех кандидатов
+# • get_candidate(candidate_id) - возвращает одного кандидата по его id
+# • get_candidates_by_name(candidate_name) — возвращает кандидатов по имени
+# • get_candidates_by_skill(skill_name) - возвращает кандидатов по навыку
+
+def load_candidates_from_json(path):
+    pass
+
+def get_candidates(candidate_id):
+    pass
+
+def get_candidates_by_name(candidate_name):
+    pass
+
+def get_candidates_by_skill(skill_name):
+    pass
